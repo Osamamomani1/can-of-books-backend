79b3feb2c09800750dad359bf1857df23eff172a
motaser <araidahmontaser@gmail.com>
2021-07-07 14:29:11 +0300

    montaser

--- controller/User.controller.js
+++ controller/User.controller.js
@@ -1,16 +1,18 @@
 'use strict'
 const userModel= require('../models/User.model')
-const seedUser= require('../models/User.model');
-
+const seeduser = require('../models/User.model')
 const Usercontrollerfunc=(req,res)=>{
-   
+    // let obj = seeduser();
+    // res.send(obj);
+
+
     let requestedEmail=req.query.email;
     userModel.findOne({email: requestedEmail}, function(err,userData){
         if(err){
             console.log('there is an error');
         }
             res.send(userData)
-
+        
     });
 }
 
